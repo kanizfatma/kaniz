4a6fe7cea78ede79d9365f870be6391223cfd514
Kaniz Fatma <git@kanizfatma.com>
2024-01-12 19:33:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+<script>
+      var typed = new Typed('.auto-type', {
+        strings: ['UX Designer.', 'Blogger.'],
+        typeSpeed: 50,
+        backspeed: 5,
+        loop: true,
+      });
+    </script>
